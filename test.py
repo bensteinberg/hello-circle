c81df9aff59fdd09ef9bea583cff9322a4b20ca2
Ben Steinberg <bsteinberg@law.harvard.edu>
2019-04-12 13:38:56 -0400

    Try a pip install

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import sys
+import requests
 
 print("Hello, Circle CI!")
 
 if len(sys.argv) > 1:
     print("I think the secret is %s" % sys.argv[1])
+
+r = requests.get('https://lil.law.harvard.edu/')
+print(r.status_code)
+print(r.text)
